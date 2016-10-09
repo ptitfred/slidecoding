316c740befd173e02fc02146ddb7c9113c402906
Frédéric Menou <frederic.menou@gmail.com>
2016-10-09 22:45:05 +0200

    Serve a slidecoding project

--- app/Slider.hs
+++ app/Slider.hs
@@ -3,7 +3,7 @@ module Main (main) where
 import           Slidecoding             (browse, indexIO, loadExposedModules, load, processSlides, ValidationMessage, Presentation(..), Metadata(..))
 import           Slidecoding.Types
 
-import           Control.Monad           ((>=>), when)
+import           Control.Monad           ((>=>))
 import           Data.List               (isSuffixOf)
 import qualified Data.Text.IO       as T
 import           System.Environment      (getArgs)
@@ -11,7 +11,7 @@ import           System.FilePath         ((</>))
 import           System.Directory        (createDirectoryIfMissing, getDirectoryContents, doesDirectoryExist)
 
 data Config = Config FilePath Action
-data Action = Check | ProcessSlides | Index
+data Action = Check | ProcessSlides | Index | Serve
 
 main :: IO ()
 main = withConfig run
@@ -20,6 +20,7 @@ run :: Config -> IO ()
 run (Config f Check)         = check f
 run (Config f ProcessSlides) = process f
 run (Config f Index)         = index f
+run (Config f Serve)         = serve f
 
 check :: FilePath -> IO ()
 check f = do
@@ -48,6 +49,9 @@ index path = loadExposedModules path >>= maybe noModule someModules
         someModules = mapM_ indexModule
         indexModule m = indexIO m (path </> "tmp")
 
+serve :: FilePath -> IO ()
+serve f = withProject f $ \_ -> putStrLn ("Serving " ++ f)
+
 type Project = ([FilePath], [Description])
 
 withProject :: FilePath -> (Project -> IO ()) -> IO ()
@@ -59,7 +63,9 @@ loadProject f = (,) <$> loadSlides f <*> loadModules f
 onlyWithDirectory :: FilePath -> IO () -> IO ()
 onlyWithDirectory f action = do
   good <- doesDirectoryExist f
-  when good action
+  if good
+  then action
+  else putStrLn ("No slidecoding project in " ++ f)
 
 checkPresentation :: FilePath -> IO ()
 checkPresentation = load >=> printResult
@@ -117,7 +123,7 @@ printFiles (f:fs) = do
   printFiles fs
 
 usage :: IO ()
-usage = putStrLn "Usage: slider <check|process|index> directory"
+usage = putStrLn "Usage: slider <check|process|index|serve> directory"
 
 getConfig :: IO (Maybe Config)
 getConfig = parseArgs <$> getArgs
@@ -131,4 +137,5 @@ parseAction :: String -> Maybe Action
 parseAction "check"   = Just Check
 parseAction "process" = Just ProcessSlides
 parseAction "index"   = Just Index
+parseAction "serve"   = Just Serve
 parseAction _         = Nothing
