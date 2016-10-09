fcf1ca11415ea13746e43f676039439fde1b6fb6
Frédéric Menou <frederic.menou@gmail.com>
2016-10-09 02:45:12 +0200

    Contexts

--- src/Slidecoding.hs
+++ src/Slidecoding.hs
@@ -5,6 +5,15 @@ module Slidecoding
     , Metadata(..)
     , ValidationMessage
     , load
+
+    -- Types
+    , Context(..)
+    , ModuleName
+    , Name
+    , Port
+    , Stream(..)
+    , singleModuleContext
     ) where
 
 import Slidecoding.Presentation
+import Slidecoding.Types
---
+++ src/Slidecoding/Types.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE GADTs #-}
+
+module Slidecoding.Types
+    ( Context(..)
+    , ModuleName
+    , Name
+    , Port
+    , Stream(..)
+    , singleModuleContext
+    ) where
+
+import System.FilePath ((</>), (<.>))
+
+type Port = Int
+type Name = String
+type ModuleName = String
+
+singleModuleContext :: ModuleName -> Context
+singleModuleContext moduleName = Context moduleName [path] [moduleName] []
+  where path = "src" </> moduleName <.> "hs"
+
+data Stream m where
+  Stream :: Monad m => m ()             -- prepare
+                    -> m String         -- readInput
+                    -> (String -> m ()) -- writeOutput
+                    -> Stream m
+
+data Context = Context { name            :: Name         -- A name for debugging
+                       , sources         :: [FilePath]   -- Source files to load
+                       , modules         :: [ModuleName] -- Modules to import
+                       , topLevelModules :: [ModuleName] -- Modules to interpret (private elems will be available)
+                       }
