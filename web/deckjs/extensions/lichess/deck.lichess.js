7cb3e6d2f65d958bf20dd62e56f9d43192816fcc
Frédéric Menou <frederic.menou@gmail.com>
2016-11-09 18:24:14 +0100

    Disable Lichess extension if url called with ?print=true

--- web/deckjs/extensions/lichess/deck.lichess.js
+++ web/deckjs/extensions/lichess/deck.lichess.js
@@ -1,29 +1,44 @@
+var qs = (function(a) {
+  if (a == "") return {};
+  var b = {};
+  for (var i = 0; i < a.length; ++i) {
+    var p=a[i].split('=', 2);
+    if (p.length == 1) {
+      b[p[0]] = "";
+    } else {
+      b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+  }
+  return b;
+})(window.location.search.substr(1).split('&'));
 
 function inflateUrl(lichessUrl) {
   var gameId = lichessUrl.match(/lichess:\/\/(.*)/)[1];
   return "https://lichess.org/embed/" + gameId + "?theme=purple&bg=dark";
 }
 
-(function($, deck, window, undefined) {
-  var inflateLink = function() {
-    var $link = $(this);
-    if ($link.attr('href').startsWith('lichess://')) {
-      var url = inflateUrl($link.attr('href'));
-      var $game = $("<iframe/>", { width: 926, height: 600, src: url });
-      if ($link.text() !== "") {
-        var $wrapper = $('<div/>');
-        var $text = $('<p>', { html: $link.html() });
-        $wrapper.append($text).append($game);
-        $link.replaceWith($wrapper);
-      } else {
-        $link.replaceWith($game);
+if (qs.print !== "true") {
+  (function($, deck, window, undefined) {
+    var inflateLink = function() {
+      var $link = $(this);
+      if ($link.attr('href').startsWith('lichess://')) {
+        var url = inflateUrl($link.attr('href'));
+        var $game = $("<iframe/>", { width: 926, height: 600, src: url });
+        if ($link.text() !== "") {
+          var $wrapper = $('<div/>');
+          var $text = $('<p>', { html: $link.html() });
+          $wrapper.append($text).append($game);
+          $link.replaceWith($wrapper);
+        } else {
+          $link.replaceWith($game);
+        }
       }
-    }
-  };
+    };
 
-  var embedLichess = function() {
-    $.each($('a[href]'), inflateLink);
-  };
+    var embedLichess = function() {
+      $.each($('a[href]'), inflateLink);
+    };
 
-  $(document).bind('deck.beforeInit', embedLichess);
-})(jQuery, 'deck', this);
+    $(document).bind('deck.beforeInit', embedLichess);
+  })(jQuery, 'deck', this);
+}
